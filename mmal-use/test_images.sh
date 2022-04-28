47fcb83c6caaf1024894635607b11a84411e1228
Uitaek Hong <dmlxor8422@gmail.com>
2022-04-28 15:21:58 +0900

    sample images are here

--- mmal-use/test_images.sh
+++ mmal-use/test_images.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # echo &PWD
 
-# dir_path="/home/pi/dir-1/n01930112"
-dir_path="/home/hong/dir6/playground/n01930112"
+dir_path="/home/pi/dir-1/n01930112"
+# dir_path="/home/hong/dir6/playground/n01930112"
 
 for entry in ${dir_path}/*
 do
